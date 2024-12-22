0d8a4735034a5548b562436ccc3d679c9f71e721
renatxat <khatymov.rr@phystech.edu>
2024-12-22 11:46:10 +0300

    add proxying for final class and class without default constructor

--- src/main/java/hw2task1/Main.java
+++ src/main/java/hw2task1/Main.java
@@ -7,7 +7,17 @@ public class Main {
   public static void main(String[] args)
       throws InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
     A a = new A();
-    A cachedA = Utils.cache(a);
+    AInterface cachedA = (AInterface) Utils.cache(a);
+
+    cachedA.setValue("Hello");
+    System.out.println(cachedA.mainMethod());
+    // Ожидается: "original method" и "Hello"
+    System.out.println(cachedA.mainMethod());
+    // Не выводит "original method", ожидается кэшированный "Hello"
+
+
+    AFinal af = new AFinal("not_empty_string");
+    cachedA = (AInterface) Utils.cache(af);
 
     cachedA.setValue("Hello");
     System.out.println(cachedA.mainMethod());
